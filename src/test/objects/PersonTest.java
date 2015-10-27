a2929fddaea67d82b1d3431ce5679735eca8673c
Austin Clark <austin.clark@hpe.com>
2015-10-27 17:12:08 -0600

    Add unit tests skeleton for objects

---
+++ src/test/objects/PersonTest.java
@@ -0,0 +1,105 @@
+package test.objects; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* Person Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Oct 27, 2015</pre> 
+* @version 1.0 
+*/ 
+public class PersonTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: setName(String name) 
+* 
+*/ 
+@Test
+public void testSetName() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getName() 
+* 
+*/ 
+@Test
+public void testGetName() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getAddresses() 
+* 
+*/ 
+@Test
+public void testGetAddresses() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getPhoneNumbers() 
+* 
+*/ 
+@Test
+public void testGetPhoneNumbers() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setPhoneNumbers(ArrayList<Phone> phones) 
+* 
+*/ 
+@Test
+public void testSetPhoneNumbers() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getAddress(int index) 
+* 
+*/ 
+@Test
+public void testGetAddress() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: addAddress(Address address) 
+* 
+*/ 
+@Test
+public void testAddAddress() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setAddresses(ArrayList<Address> addresses) 
+* 
+*/ 
+@Test
+public void testSetAddresses() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
---
+++ src/test/objects/PizzaTest.java
@@ -0,0 +1,215 @@
+package test.objects; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* Pizza Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Oct 27, 2015</pre> 
+* @version 1.0 
+*/ 
+public class PizzaTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: toString() 
+* 
+*/ 
+@Test
+public void testToString() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: equals(Object o) 
+* 
+*/ 
+@Test
+public void testEquals() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getToppingList() 
+* 
+*/ 
+@Test
+public void testGetToppingList() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setToppingList(ArrayList<Topping> toppings) 
+* 
+*/ 
+@Test
+public void testSetToppingList() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getSauce() 
+* 
+*/ 
+@Test
+public void testGetSauce() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setSauce(Sauce sauce) 
+* 
+*/ 
+@Test
+public void testSetSauce() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getSize() 
+* 
+*/ 
+@Test
+public void testGetSize() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setSize(PizzaSize size) 
+* 
+*/ 
+@Test
+public void testSetSize() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getStatus() 
+* 
+*/ 
+@Test
+public void testGetStatus() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setStatus(PIZZA_STATUS status) 
+* 
+*/ 
+@Test
+public void testSetStatus() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: sendPizzaToMakeline() 
+* 
+*/ 
+@Test
+public void testSendPizzaToMakeline() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: loadPizza() 
+* 
+*/ 
+@Test
+public void testLoadPizza() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: completePizza() 
+* 
+*/ 
+@Test
+public void testCompletePizza() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: calculatePrice() 
+* 
+*/ 
+@Test
+public void testCalculatePrice() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getPrice() 
+* 
+*/ 
+@Test
+public void testGetPrice() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setPrice(double price) 
+* 
+*/ 
+@Test
+public void testSetPrice() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setOrderID(int orderID) 
+* 
+*/ 
+@Test
+public void testSetOrderID() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getOrderID() 
+* 
+*/ 
+@Test
+public void testGetOrderID() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: makelineToString() 
+* 
+*/ 
+@Test
+public void testMakelineToString() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
